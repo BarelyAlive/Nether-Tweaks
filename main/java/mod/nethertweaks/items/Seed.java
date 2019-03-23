8fe76d0d4b013dfb66b7c89607903c22104d00a5
Gaprosys <gaprosys@gmail.com>
2019-03-23 12:35:12 +0100

    Recovered Seed functionality

--- main/java/mod/nethertweaks/items/Seed.java
+++ main/java/mod/nethertweaks/items/Seed.java
@@ -20,48 +20,50 @@ import net.minecraft.world.World;
 public class Seed extends CustomItem{
 	
 	public Seed(String name) {
-		super(null, 64, NetherTweaksMod.tabNetherTweaksMod, false, 5, name);
+		super(null, 64, NetherTweaksMod.tabNetherTweaksMod, false, 0, name);
 	}
 
 	/**
 	 * Called when a Block is right-clicked with this Item
 	 */
+	@Override
 	public EnumActionResult onItemUse(EntityPlayer player, World worldIn, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ)
 	{
-		RayTraceResult rtr = new RayTraceResult(player);
-		Block block = worldIn.getBlockState(rtr.getBlockPos()).getBlock();
-		pos = rtr.getBlockPos();
+		Block block = worldIn.getBlockState(pos).getBlock(); //worldIn.getBlockState(rtr.getBlockPos()).getBlock();
 		
-		if(this.getUnlocalizedName() == INames.MUSHROOMSPORES){
-		
-				if(block == Blocks.DIRT || block == Blocks.GRASS){
-		
-					worldIn.setBlockState(pos, Blocks.MYCELIUM.getDefaultState());
-				}
-		
+		if(this.getUnlocalizedName().equals("item." + INames.MUSHROOMSPORES))
+		{
+			if(block == Blocks.DIRT || block == Blocks.GRASS){
+				worldIn.setBlockState(pos, Blocks.MYCELIUM.getDefaultState());
 			}
-		
-			if(this.getUnlocalizedName() == INames.SEEDGRASS){
-			
-				if(block == Blocks.DIRT){
-					worldIn.setBlockState(pos, Blocks.GRASS.getDefaultState());
+		}
+		else if(this.getUnlocalizedName().equals("item." + INames.SEEDGRASS))
+		{
+			if(block == Blocks.DIRT){
+				worldIn.setBlockState(pos, Blocks.GRASS.getDefaultState());
+			}
+		}
+		else if(this.getUnlocalizedName().equals("item." + INames.CACTUSSEED))
+		{
+			Block block1 = worldIn.getBlockState(pos.add(1, 1, 0)).getBlock();
+			Block block2 = worldIn.getBlockState(pos.add(0, 1, 1)).getBlock();
+			Block block3 = worldIn.getBlockState(pos.add(-1, 1, 0)).getBlock();
+			Block block4 = worldIn.getBlockState(pos.add(0, 1, -1)).getBlock();
+			Block block5 = worldIn.getBlockState(pos.add(0, 1, 0)).getBlock();
+			if(block == Blocks.SAND)
+			{
+				if( block1.equals(Blocks.AIR)
+				 && block2.equals(Blocks.AIR)
+				 && block3.equals(Blocks.AIR)
+				 && block4.equals(Blocks.AIR)
+				 && block5.equals(Blocks.AIR)
+				)
+				{
+					worldIn.setBlockState(pos.add(0, 1, 0), Blocks.CACTUS.getDefaultState());
 				}
 			}
-			
-			if(this.getUnlocalizedName() == INames.CACTUSSEED){
-				Block block1 = worldIn.getBlockState(pos.add(1, 1, 0)).getBlock();
-				Block block2 = worldIn.getBlockState(pos.add(0, 1, 1)).getBlock();
-				Block block3 = worldIn.getBlockState(pos.add(-1, 1, 0)).getBlock();
-				Block block4 = worldIn.getBlockState(pos.add(0, 1, -1)).getBlock();
-				Block block5 = worldIn.getBlockState(pos.add(0, 1, 0)).getBlock();
-				if(block == Blocks.SAND){
-					if(block1.equals(Blocks.AIR) && block2.equals(Blocks.AIR) && block3.equals(Blocks.AIR) && block4.equals(Blocks.AIR)
-							&& block5.equals(Blocks.AIR)){
-						worldIn.setBlockState(pos.add(0, 1, 0), Blocks.CACTUS.getDefaultState());
-					}
-				}
-			}
+		}
 		
-			return EnumActionResult.SUCCESS;
+		return EnumActionResult.SUCCESS;
 	}
 }
