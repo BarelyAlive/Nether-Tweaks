ae1019e8ec7c38421b31a55b9bd662a8639b3d09
Angayoco <angayoco@protonmail.com>
2019-04-22 12:45:38 +0200

    Name & id Change

--- main/java/mod/nethertweaks/Konstanten.java
+++ main/java/mod/nethertweaks/Konstanten.java
@@ -8,15 +8,14 @@ import net.minecraft.item.ItemStack;
 public class Konstanten {
 	
 	//itemBase
-	public static final ItemStack CRUSHEDGOLD = new ItemStack(ItemHandler.ITEMBASE, 1, 0);
-	public static final ItemStack CRUSHEDIRON = new ItemStack(ItemHandler.ITEMBASE, 1, 1);
-	public static final ItemStack ENDBOX = new ItemStack(ItemHandler.ITEMBASE, 1, 2);
-	public static final ItemStack PORTALCORE = new ItemStack(ItemHandler.ITEMBASE, 1, 3);
-	public static final ItemStack ENDERINFUSEDFRAME = new ItemStack(ItemHandler.ITEMBASE, 1, 4);
-	public static final ItemStack HELLFAYAH = new ItemStack(ItemHandler.ITEMBASE, 1, 5);
-	public static final ItemStack POWDEROFLIGHT = new ItemStack(ItemHandler.ITEMBASE, 1, 6);
-	public static final ItemStack SALT = new ItemStack(ItemHandler.ITEMBASE, 1, 7);
-	public static final ItemStack STONEBAR = new ItemStack(ItemHandler.ITEMBASE, 1, 8);
+	public static final ItemStack ENDBOX = new ItemStack(ItemHandler.ITEMBASE, 1, 0);
+	public static final ItemStack PORTALCORE = new ItemStack(ItemHandler.ITEMBASE, 1, 1);
+	public static final ItemStack ENDERINFUSEDFRAME = new ItemStack(ItemHandler.ITEMBASE, 1, 2);
+	public static final ItemStack HELLFAYAH = new ItemStack(ItemHandler.ITEMBASE, 1, 3);
+	public static final ItemStack POWDEROFLIGHT = new ItemStack(ItemHandler.ITEMBASE, 1, 4);
+	public static final ItemStack SALT = new ItemStack(ItemHandler.ITEMBASE, 1, 5);
+	public static final ItemStack STONEBAR = new ItemStack(ItemHandler.ITEMBASE, 1, 6);
+	public static final ItemStack STRING = new ItemStack(ItemHandler.ITEMBASE, 1, 7);
 	
 	
 	//ItemStack Ore Material
