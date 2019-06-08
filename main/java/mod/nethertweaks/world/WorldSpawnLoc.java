0e7e0ef3702229eb66b337681b7b211ec0467253
Angayoco <angayoco@protonmail.com>
2019-06-08 18:15:01 +0200

    Update WorldSpawnLoc.java

--- main/java/mod/nethertweaks/world/WorldSpawnLoc.java
+++ main/java/mod/nethertweaks/world/WorldSpawnLoc.java
@@ -4,11 +4,26 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
+import javax.annotation.Nonnull;
+
 import mod.sfhcore.vars.PlayerPosition;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.math.BlockPos;
 
 public class WorldSpawnLoc {
 	
+	//The initial Location when entering a world
+	private static PlayerPosition initialPosition;
+	
+	public static PlayerPosition getInitialPosition() {
+		return initialPosition;
+	}
+
+	public static void setInitialPosition(@Nonnull PlayerPosition pos) {
+		initialPosition = pos;
+	}
+
+	//This is for the players™ and their individual spawns locations
 	public static Map<UUID, PlayerPosition> spawnLocas = new HashMap<UUID, PlayerPosition>();
 	
 	public static void setSpawnLocations(Map<UUID, PlayerPosition> map)
@@ -19,6 +34,38 @@ public class WorldSpawnLoc {
 	public static Map<UUID, PlayerPosition> getSpawnLocations()
 	{
 		return spawnLocas;
+	}
+	
+	public static PlayerPosition setNewSpawnLocation(@Nonnull EntityPlayer player)
+	{
+		UUID playerID = player.getUUID(player.getGameProfile());
+		
+		if(playerID == null) playerID = player.getOfflineUUID(player.getName());
+		
+		if(spawnLocas.containsKey(playerID))
+		{
+			spawnLocas.replace(playerID, new PlayerPosition(player.getPosition()));
+		}
+		else
+			spawnLocas.put(playerID, new PlayerPosition(player.getPosition()));
+		
+		return new PlayerPosition(player.getPosition());
+	}
+	
+	public static PlayerPosition resetSpawnLocation(@Nonnull EntityPlayer player)
+	{
+		UUID playerID = player.getUUID(player.getGameProfile());
+		
+		if(playerID == null) playerID = player.getOfflineUUID(player.getName());
+		
+		if(spawnLocas.containsKey(playerID))
+		{
+			PlayerPosition pos = spawnLocas.get(playerID);
+			pos = getInitialPosition();
+			return pos;
+		}
+		
+		return new PlayerPosition(player.getPosition());
 	}
 	
 }
