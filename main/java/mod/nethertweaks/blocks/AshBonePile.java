d19a3b891400b676d9230c2e456f34cb83d55284
Angayoco <angayoco@protonmail.com>
2019-07-20 15:50:56 +0200

    Update AshBonePile.java

--- main/java/mod/nethertweaks/blocks/AshBonePile.java
+++ main/java/mod/nethertweaks/blocks/AshBonePile.java
@@ -16,13 +16,17 @@ import net.minecraft.block.properties.PropertyBool;
 import net.minecraft.block.properties.PropertyDirection;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumBlockRenderType;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.NonNullList;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
 public class AshBonePile extends CubeContainerHorizontal
@@ -57,9 +61,24 @@ public class AshBonePile extends CubeContainerHorizontal
 	}
 	
 	@Override
+	public String getUnlocalizedName() {
+		return this.lightValue == 8 ? "tile.bonfire" : "tile.ash_bone_pile";
+	}
+	
+	@Override
     public int getLightValue(IBlockState state)
 	{        
         return state.getValue(LIT) ? 8 : 0;
+	}
+	
+	@Override
+	public void getDrops(NonNullList<ItemStack> drops, IBlockAccess world, BlockPos pos, IBlockState state,
+			int fortune)
+	{
+		if(state.getValue(LIT))
+			drops.add(new ItemStack(ItemHandler.COILED_SWORD));
+		
+		super.getDrops(drops, world, pos, state, fortune);
 	}
 	
 	@Override
