5a10dc227c3d1218407bb9ea4e946cefdf1274a8
Angayoco <angayoco@protonmail.com>
2019-09-08 19:56:46 +0200

    Update NTM.java

--- main/java/mod/nethertweaks/recipes/defaults/NTM.java
+++ main/java/mod/nethertweaks/recipes/defaults/NTM.java
@@ -202,7 +202,7 @@ public class NTM implements IRecipeDefaults
 	{
 		registry.register(TankUtil.WATER_BOTTLE, 3, 1.4f, 0f);
 		registry.register(new ItemInfo(Items.MILK_BUCKET), 8, 3.4f, 0f);
-		registry.register(new ItemInfo(Items.MUSHROOM_STEW), 7, 1.2f, 0f);
+		registry.register(new ItemInfo(Items.MUSHROOM_STEW), 7, 1.2f, 0.4f);
 		registry.register(new ItemInfo(Items.RABBIT_STEW), 7, 2.0f, 0f);
 		registry.register(new ItemInfo(Items.BEETROOT_SOUP), 8, 1.2f, 0f);
 		registry.register(new ItemInfo(ItemHandler.CANTEEN), 3, 1.4f, 0f);
